45266a156bee85a3f5a8bf9e8e4200173824666a
westernpan308 <131693051+westernpan308@users.noreply.github.com>
2023-05-01 17:53:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+while True:
+  input("Please enter a File name: ")
+  name = (input("\nPlease enter your First and last name: "))
+  street = (input("\nPlease enter your street name: "))
+  phone = (input("\nPlease enter your phone number including your area code: "))
+  #empty line (line 6 for a cleaner look)
+  print ()
+  print(name, street, phone)
+  
+  print("\nwould you like to add another File")
+  option = input("y/n > ")
+    
+  if option == "y" or option == "Y":
+      continue
+      
+  elif option == "n" or option == "N":
+      print("Thank you and have a great day please return for more weather updates")
+      break
+  else:
+      print("\nError invalid entry please try again with Y or N\n ")
+      continue
