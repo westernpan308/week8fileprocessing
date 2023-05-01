7ec7c4820d24b3bc7f51609112e16ecce402926f
westernpan308 <131693051+westernpan308@users.noreply.github.com>
2023-05-01 22:58:57 +0000

    saved changed

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-def main():
+while True:
+  input("Please enter a File name: ")
+  name = (input("\nPlease enter your First and last name: "))
+  street = (input("\nPlease enter your street name: "))
+  phone = (input("\nPlease enter your phone number including your area code: "))
+  #empty line (line 6 for a cleaner look)
+  print ()
+  print(""+name+", "+street+", "+phone+"")
   
-  while True:
-    input("Please enter a File name: ")
-    name = (input("\nPlease enter your First and last name: "))
-    street = (input("\nPlease enter your street name: "))
-    phone = (input("\nPlease enter your phone number including your area code: "))
-    #empty line (line 6 for a cleaner look)
-    print ()
-    print(""+name+", "+street+", "+phone+"")
-    
-    print("\nwould you like to add another File")
-    option = input("y/n > ")
+  print("\nwould you like to add another File")
+  option = input("y/n > ")
       
-    if option == "y" or option == "Y":
-      continue
+  if option == "y" or option == "Y":
+    print()
         
-    elif option == "n" or option == "N":
-      print("\nThank you and have a great day")
-      break
-    else:
-      input("\nError invalid entry please try again with Y or N\n ")
-    main
+  elif option == "n" or option == "N":
+    print("\nThank you and have a great day")
+    break
+  
+  else:
+    print("\nError invalid entry please try again with Y or N\n ")
+      
+        
+   
